30963db25d9d20fa0c91e0b3a12a3c3b696bf232
Pribylov-Maksim <pribylov-maksim@users.noreply.github.com>
2020-05-02 18:05:03 +0300

    Update main.c
    Summary

--- hw_02_dynamic_array_protection/main.c
+++ hw_02_dynamic_array_protection/main.c
@@ -5,91 +5,136 @@
 #include <malloc.h>
 int main()
 {
-	int* M = NULL;
-	int* N = NULL;
-	int* A = NULL;
-	int n, m, i;
-	int p, j = 0;
-
+	int* a = NULL;
+	int* b = NULL;
+	int* S = NULL;
+	int n, m, i, j, p, k=0, counter=0;
 	printf("n=");
 	scanf("%d", &n);
 	printf("m=");
 	scanf("%d", &m);
-
 	p = n + m;
-	M = (int*)malloc(m * sizeof(int));
-	N = (int*)malloc(n * sizeof(int));
-	A = (int*)malloc(p * sizeof(int));
+	a = (int*)malloc(n * sizeof(int));
+	b = (int*)malloc(m * sizeof(int));
+	S = (int*)malloc(p * sizeof(int));
 
-
-
-	for (int i = 0; i < n; i++)
+	printf("Enter numbers for array a: \n");
+	for (i = 0; i < n; i++)
 	{
-		scanf("%d", &N[i]);
+		scanf("%d", &(*(a + i)));
 	}
-
-	for (int i = 0; i < m; i++)
+	printf("Enter numbers for array b: \n");
+	for (j = 0; j < m; j++)
 	{
-		scanf("%d", &M[i]);
+		scanf("%d", &(*(b + j)));
 	}
-	if (n < m)
+	i = 0;
+	j = 0;
+	if (n > m)
 	{
-		for (int i = 0; i < n; i++)
+		while ((i < n) && (j < m))
 		{
-			A[i] = N[i];
-		}
-		for (int i = n; i < p; i++)
-		{
-			A[i] = M[j];
-			j++;
-		}
-	}
-	if (m < n)
-	{
-		for (int i = 0; i < m; i++)
-		{
-			A[i] = M[i];
-		}
-		for (int i = m; i < p; i++)
-		{
-			A[i] = N[j];
-			j++;
-		}
-	}
-
-	for (i = 0; i < p - 1; i++)
-	{
-       for (j = 0; j < p - i - 1; j++)
-		{
-			if (A[j] > A[j + 1])
+			if (*(a + i) < *(b + j))
 			{
-				int tmp = A[j];
-				A[j] = A[j + 1];
-				A[j + 1] = tmp;
+				*(S + k) = *(a + i);
+				i++;
+				k++;
+				counter++;
+			}
+			else if (*(a + i) == *(b + j))
+			{
+				*(S + k) = *(a + i);
+				i++;
+				j++;
+				k++;
+				counter++;
+			}
+			else
+			{
+				*(S + k) = *(b + j);
+				j++;
+				k++;
+				counter++;
+			}
+		}
+		while (i < n)
+		{
+			*(S + k) = *(a + i);
+			i++;
+			k++;
+			counter++;
+		}
+	}
+	else if (n < m)
+	{
+		while ((i < n) && (j < m))
+		{
+			if (*(a + i) < *(b + j))
+			{
+				*(S + k) = *(a + i);
+				i++;
+				k++;
+				counter++;
+			}
+			else if (*(a + i) == *(b + j))
+			{
+				*(S + k) = *(a + i);
+				i++;
+				j++;
+				k++;
+				counter++;
+			}
+			else
+			{
+				*(S + k) = *(b + j);
+				j++;
+				k++;
+				counter++;
+			}
+		}
+		while (j < m)
+		{
+			*(S + k) = *(b + j);
+			j++;
+			k++;
+			counter++;
+		}
+	}
+	else if (n == m)
+	{
+		while ((i < n) && (j < m))
+		{
+			if (*(a + i) < *(b + j))
+			{
+				*(S + k) = *(a + i);
+				i++;
+				k++;
+				counter++;
+			}
+			else if (*(a + i) == *(b + j))
+			{
+				*(S + k) = *(a + i);
+				i++;
+				j++;
+				k++;
+				counter++;
+			}
+			else
+			{
+				*(S + k) = *(b + j);
+				j++;
+				k++;
+				counter++;
 			}
 		}
 	}
-
-
-	for (i = 0; i < p - 1; i++)
-		if (A[i] == A[i + 1])
-		{
-			for (j = i; j < p - 1; j++)
-				A[j] = A[j + 1];
-			p -= 1;
-		}
-
-	for (i = 0; i < p; i++)
+	for (k = 0; k < counter; k++)
 	{
-		printf("%d ", A[i]);
+		printf("%d ", *(S + k));
 	}
-
-	while (getchar() != '\n');
 	getchar();
-
-
-	free(M);
-	free(N);
-
+	free(a);
+	free(b);
+	free(S);
 	return 0;
 }
