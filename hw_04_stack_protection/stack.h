d72fdc4bf8ecea81da2ab0a9612f1bf0b58f949f
Pribylov-Maksim <pribylov-maksim@users.noreply.github.com>
2020-05-16 16:29:03 +0300

    Create stack.h
    Summary

---
+++ hw_04_stack_protection/stack.h
@@ -0,0 +1,13 @@
+#ifndef STACK_H
+#define STACK_H
+struct stack
+{
+	char value;
+	struct stack* next;
+};
+typedef struct stack stack;
+input(stack** head, const char str);
+char pop(stack** head);
+empty(stack** head);
+int count(stack** head);
+#endif
