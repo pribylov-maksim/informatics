bb164e0e9f178faffb113bd060af9a0408523724
Pribylov-Maksim <pribylov-maksim@users.noreply.github.com>
2020-05-16 14:56:01 +0300

    Create stack.c
    Summary

---
+++ hw_04_stack/stack.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "stack.h"
+#include <stdlib.h>
+#include <string.h>
+input(stack** head, const char str)
+{
+	stack* s = (stack*)malloc(sizeof(stack));
+	s->value = str;
+	s->next = *head;
+	*head = s;
+}
+empty(stack** head)
+{
+	if (*head == NULL)
+		return 1;
+	else
+		return 0;
+}
+char pop(stack** head)
+{
+	if (*head == NULL)
+		return 0;
+	else
+	{
+		stack* s = NULL;
+		stack* p = NULL;
+		p = *head;
+		char result = p->value;
+		*head = p->next;
+		free(p);
+		s = *head;
+		return result;
+	}
+}
